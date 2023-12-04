cfe168c29227f0988daf3159562712ad99a1384c
007 <21ita007@itbspa.de>
2023-12-04 14:11:53 +0100

    IT-P 04.12.23

--- IT-P/EMS_2024/ManageEventsApp/ManageEventsWindow.xaml.cs
+++ IT-P/EMS_2024/ManageEventsApp/ManageEventsWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -20,14 +21,38 @@ namespace ManageEventsApp
     /// </summary>
     public partial class ManageEventsWindow : Window
     {
+        private ManageEvents.ManageEventsDataset dsEvents;
+
+        private ICollectionView eventView;
+        private ICollectionView eventDatenView;
+
         public ManageEventsWindow()
         {
             InitializeComponent();
+
+            dsEvents = (ManageEvents.ManageEventsDataset)(FindResource("manageEventsDataset"));
+
+            eventView = ((CollectionViewSource)this.FindResource("tbl_EventsViewSource")).View;
+            eventDatenView = ((CollectionViewSource)this.FindResource("tbl_Events_tbl_EventDatenViewSource")).View;
         }
 
         private void beendenMenueItem_Click(object sender, RoutedEventArgs e)
         {
             Close();
+        }
+
+        private void RefreshBindings()
+        {
+            et_BezeichnungListBox.SelectedIndex = 0;
+
+            //Binden der Elterntabellen
+            Binding vBinding = new Binding();
+            vBinding.Source = eventView;
+            vBinding.Path = new PropertyPath("ev_EvVeranstalterID");
+
+            cboVeranstalter.ItemsSource = dsEvents.tbl_EvVeranstalter;
+            cboVeranstalter.DisplayMemberPath = "ev_Firma";
+            cboVeranstalter.SelectedValuePath = "ev_EvVeranstalterID";
         }
     }
 }
