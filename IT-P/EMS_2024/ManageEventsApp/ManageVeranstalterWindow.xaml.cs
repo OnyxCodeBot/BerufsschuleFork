3d2f4099baca354f1e914be3842557334a6f7c1c
007 <21ita007@itbspa.de>
2024-01-15 14:16:54 +0100

    IT-P 15.01.2024

--- IT-P/EMS_2024/ManageEventsApp/ManageVeranstalterWindow.xaml.cs
+++ IT-P/EMS_2024/ManageEventsApp/ManageVeranstalterWindow.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -44,6 +45,98 @@ namespace ManageEventsApp
 
             //Aktuell gewählten eventveranstalter setzen
             veranstalterView.MoveCurrentToPosition(index);
+        }
+
+        private void ShowErrorMessages(object sender, RoutedEventArgs e)
+        {
+            MessageBoxResult mr = MessageBox.Show(((Exception)sender).Message
+                + "\nVernanstalter, Straße, Hausnummer, Postleitzahl und Ort "
+                + "müssen Werte enthalten.\n"
+                + "Möchten Sie die fehlenden Einträge ergänzen?",
+                "Fehler",
+                MessageBoxButton.YesNo,
+                MessageBoxImage.Error);
+
+            //Abbrechen oder Überarbeiten?
+            if(mr == MessageBoxResult.No)
+            {
+                btnAbbrechen.RaiseEvent(e);
+            }
+            else
+            {
+                ev_FirmaTextBox.Focus();
+            }
+        }
+
+        private void btnNeuerVeranstalter_Click(object sender, RoutedEventArgs e)
+        {
+            //Buttons bei Bearbeitung sperren / freigeben
+            veranstalterNavBar.IsEnabled = false;
+
+            btnNeuerVeranstalter.IsEnabled = false;
+            btnSchliessen.IsEnabled = false;
+
+            btnAbbrechen.IsEnabled = true;
+            btnUebernehmen.IsEnabled = true;
+
+            try
+            {
+                //Neue Row erzeugen
+                ManageEvents.ManageEventsDataset.tbl_EvVeranstalterRow verRow = 
+                    (ManageEvents.ManageEventsDataset.tbl_EvVeranstalterRow)dsVeranstalter.tbl_EvVeranstalter.NewRow();
+
+                //Überprüfung der Contraints zum Bearbeiten abschalten
+                dsVeranstalter.EnforceConstraints = false;
+
+                //Row hinzufügen und Anzeige updaten
+                dsVeranstalter.tbl_EvVeranstalter.Rows.Add(verRow);
+
+                veranstalterView.MoveCurrentToLast();
+
+                //Bearbeitung der Zeile beginnen
+                verRow.BeginEdit();
+
+                ev_FirmaTextBox.Focus();
+            }
+            catch(Exception ex)
+            {
+                ShowErrorMessages(ex, e);
+            }
+        }
+
+        private void btnAbbrechen_Click(object sender, RoutedEventArgs e)
+        {
+            //Neue Row wieder löschen
+            dsVeranstalter.tbl_EvVeranstalter.Rows.Remove(((DataRowView)veranstalterView.CurrentItem).Row);
+
+            //Constraints wieder herstellen
+            dsVeranstalter.EnforceConstraints = true;
+
+            //Button nach Bearbeitung sperren/freigeben
+            veranstalterNavBar.IsEnabled = true;
+
+            btnNeuerVeranstalter.IsEnabled = true;
+            btnSchliessen.IsEnabled = true;
+
+            btnAbbrechen.IsEnabled = false;
+            btnUebernehmen.IsEnabled = false;
+        }
+
+        private void btnUebernehmen_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                //Daten an Dataset senden
+                veranstalterBindingGroup.UpdateSources();
+
+                //aktuelle Bearbeitung beenden
+                ((DataRowView)veranstalterView.CurrentItem).Row.EndEdit();
+
+                //Überprüfung der constraints wieder zuschalten
+                dsVeranstalter.EnforceConstraints = true;
+
+                //TODO
+            }
         }
     }
 }
