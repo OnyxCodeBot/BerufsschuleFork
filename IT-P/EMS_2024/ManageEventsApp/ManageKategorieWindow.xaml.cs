458dd5c15cbf3397de9043cb7c96abf04e2d1a4f
Tredex <marius.rossgotterer@icloud.com>
2024-01-17 19:34:33 +0100

    IT-P 17.01.2024
    Fleißarbeit Zuhause für die 1

--- IT-P/EMS_2024/ManageEventsApp/ManageKategorieWindow.xaml.cs
+++ IT-P/EMS_2024/ManageEventsApp/ManageKategorieWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -41,6 +42,114 @@ namespace ManageEventsApp
 
             //Auf gewählte Kategorie aus ManageEventsWindow springen
             kategorieView.MoveCurrentToPosition(index);
+        }
+
+        private void ShowErrorMessages(object sender, RoutedEventArgs e)
+        {
+            MessageBoxResult mr = MessageBox.Show(((Exception)sender).Message
+                + "\nKategorie muss einen Wert enthalten!"
+                + "\nMöchten Sie die fehlenden Einträge ergänzen?",
+                "Fehler",
+                MessageBoxButton.YesNo,
+                MessageBoxImage.Error);
+
+            //Abbrechen oder Überarbeiten?
+            if (mr == MessageBoxResult.No)
+            {
+                btnAbbrechen.RaiseEvent(e);
+            }
+            else
+            {
+                ek_KatBezeichnungTextBox.Focus();
+            }
+        }
+
+        private void btnNeueKategorie_Click(object sender, RoutedEventArgs e)
+        {
+            //Sperren der Steuerelemente
+            kategorieNavBar.IsEnabled = false;
+
+            btnNeueKategorie.IsEnabled = false;
+            btnSchliessen.IsEnabled = false;
+
+            btnUebernehmen.IsEnabled = true;
+            btnAbbrechen.IsEnabled = true;
+
+            try
+            {
+                ManageEvents.ManageEventsDataset.tbl_EvKategorieRow katRow =
+                    (ManageEvents.ManageEventsDataset.tbl_EvKategorieRow)dsKategorie.tbl_EvKategorie.NewRow();
+
+                dsKategorie.EnforceConstraints = false;
+
+                dsKategorie.tbl_EvKategorie.Rows.Add(katRow);
+
+                kategorieView.MoveCurrentToLast();
+
+                katRow.BeginEdit();
+
+                ek_KatBezeichnungTextBox.Focus();
+            }
+            catch (Exception ex)
+            {
+                ShowErrorMessages(ex, e);
+            }
+        }
+
+        private void btnUebernehmen_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                ((DataRowView)kategorieView.CurrentItem).Row.EndEdit();
+
+                dsKategorie.EnforceConstraints = true;
+
+                kategorieNavBar.NavSourceView = (CollectionView)kategorieView;
+
+                kategorieNavBar.IsEnabled = true;
+
+                btnNeueKategorie.IsEnabled = true;
+                btnSchliessen.IsEnabled = true;
+
+                btnUebernehmen.IsEnabled = false;
+                btnAbbrechen.IsEnabled = false;
+            }
+            catch (Exception ex)
+            {
+                ShowErrorMessages(ex, e);
+            }
+        }
+
+        private void btnAbbrechen_Click(object sender, RoutedEventArgs e)
+        {
+            dsKategorie.tbl_EvKategorie.Rows.Remove(((DataRowView)kategorieView.CurrentItem).Row);
+
+            dsKategorie.EnforceConstraints = true;
+
+            kategorieNavBar.IsEnabled = true;
+
+            btnNeueKategorie.IsEnabled = true;
+            btnSchliessen.IsEnabled = true;
+
+            btnUebernehmen.IsEnabled = false;
+            btnAbbrechen.IsEnabled = false;
+        }
+
+        private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            try
+            {
+                ((DataRowView)kategorieView.CurrentItem).Row.EndEdit();
+            }
+            catch (Exception ex)
+            {
+                ShowErrorMessages(ex, new RoutedEventArgs());
+            }
+        }
+
+        private void btnSchliessen_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
         }
     }
 }
